e0efc02d268859d9531e34bc2ca46add57843afc
Nilesh <parkhenilesh@outlook.com>
2024-04-01 11:49:14 +0530

    Fix XCode Install Confirmation

--- macOS.sh
+++ macOS.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env zsh
-
-xcode-select --install
+#!/usr/bin zsh
 
 echo "Complete the installation of Xcode Command Line Tools before proceeding."
 echo "Press enter to continue..."
 read
+
+xcode-select --install
 
 # Set scroll as traditional instead of natural
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false && killall Finder
